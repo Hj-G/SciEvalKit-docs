5470c28d98374f217d5f4d58f243980d66d306a2
Hj-G <hengjiang@sjtu.edu.cn>
2026-01-06 17:17:19 +0800

    enable markdown tables

--- docs/zh_CN/source/conf.py
+++ docs/zh_CN/source/conf.py
@@ -15,7 +15,8 @@ release = '0.1.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark',"sphinx-markdown-tables",]
+# extensions = ['recommonmark',"sphinx-markdown-tables",]
+extensions = ['myst_parser',]
 
 templates_path = ['_templates']
 exclude_patterns = []
